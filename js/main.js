5a2999c1084f6063173b541c1c3c9f3836b3a3fe
Eli <eliliao1990@gmail.com>
2017-09-29 22:42:36 +0800

    Kicho Project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	// header //
+	// $('.slick').slick({
+	//   dots: false,
+	//   arrow: false,
+	//   infinite: true,
+	//   speed: 300,
+	//   slidesToShow: 1,
+	//   centerMode: true
+	// });
+
+})
